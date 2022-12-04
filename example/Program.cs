b7bc073f0f0d7929fea3a96d7c1f377338141876
AlekseiPanov <panov.a.a60@gmail.com>
2022-12-04 14:04:01 +0300

    code +edit

---
+++ example/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая из имеющегося массива строк формирует массив из строк,
+// длинна которых меньше или равна 3 лисвола
+string[] array = new string[8] {"hello","hi","education",";-D","never","Moscow","po","1"};
+string[] GetLessThanFour(string[] Array)
+{
+    int count = 0;
+    for(int i  = 0; i < Array.Length; i++)
+    {
+        if(Array[i].Length < 4)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+    string[] lessThanFour = new string[count];
+    count = 0;
+    for(int j = 0; j < Array.Length; j++)
+    {
+        
+        if(Array[j].Length < 4)
+        {
+            lessThanFour[count] = Array[j];
+            count++;
+        }
+    }
+    return lessThanFour;
+}
+string[] NewArray = GetLessThanFour(array);
+Console.Write(String.Join(", ", NewArray));
